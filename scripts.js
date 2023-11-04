a5f589ae080b64a544c7586c701748e71946cb94
Alicia Poveda <a_poved@live.com.ar>
2023-11-04 18:33:08 -0300

    second commit

---
+++ scripts.js
@@ -0,0 +1,41 @@
+
+const ticket = 200;
+
+const descEstudiante = 0.80;
+const descTrainee = 0.50;
+const descJunior = 0.15;
+
+
+const cantidad = document.getElementById('cantidad');
+const categoria = document.getElementById('categoria');
+const totalPagar = document.getElementById('totalAPagar');
+const btnResumen = document.getElementById('resumen');
+
+
+function calcularPago() {
+
+    let total = cantidad.value * ticket;
+
+    // condicionar por descuentos
+    switch (categoria.value) {
+        case "estudiante":
+            total = total - (total * descEstudiante);
+            break;
+        case "trainee":
+            total = total - (total * descTrainee);
+            break;
+        case "junior":
+            total = total - (total * descJunior);
+            break;
+    }
+
+    totalPagar.textContent = `Total a Pagar: $ ${total}`;
+
+    console.log("hola");
+}
+
+btnResumen.addEventListener('click', (e) => {
+    e.preventDefault();
+    calcularPago();
+
+});
